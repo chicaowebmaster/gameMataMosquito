fc7cb33d8ccdb008690c90eaaafa1d8219914e05
Chico Gomes <chicaowebmaster@hotmail.com>
2025-04-23 10:45:20 -0300

    Organização do código 2

--- jogo.js
+++ jogo.js
@@ -68,22 +68,22 @@ function posicaoRandomica(){
 
   }  
 
-    var posicaoX = Math.floor(Math.random() * altura) - 90;
-	var posicaoY = Math.floor(Math.random() * largura) - 90;
+        var posicaoX = Math.floor(Math.random() * altura) - 90;
+        var posicaoY = Math.floor(Math.random() * largura) - 90;
 
-	posicaoX = posicaoX < 0 ? 0 : posicaoX;
-	posicaoY = posicaoY < 0 ? 0 : posicaoX;
+        posicaoX = posicaoX < 0 ? 0 : posicaoX;
+        posicaoY = posicaoY < 0 ? 0 : posicaoX;
 
-	// criar o elemento HTML
-	var mosquito = document.createElement('img');
-	mosquito.src = "imagens/mosca.png";
-	mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
-	mosquito.style.position = "absolute";
-	mosquito.style.left = posicaoX + "px";
-	mosquito.style.top = posicaoY + "px";
-    mosquito.id = "mosquito";
-    mosquito.onclick = function(){
-               this.remove();
+        // criar o elemento HTML
+        var mosquito = document.createElement('img');
+        mosquito.src = "imagens/mosca.png";
+        mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
+        mosquito.style.position = "absolute";
+        mosquito.style.left = posicaoX + "px";
+        mosquito.style.top = posicaoY + "px";
+        mosquito.id = "mosquito";
+        mosquito.onclick = function(){
+        this.remove();
   }
 
 	document.body.appendChild(mosquito);
